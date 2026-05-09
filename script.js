aa647654990b133e51a225bf28b331b48f660118
kvniranwb <niranjanvt967@gmail.com>
2026-05-10 01:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+
+let count = 1;
+
+// Function to add items
+function addItems(num) {
+    for (let i = 0; i < num; i++) {
+        const li = document.createElement("li");
+        li.textContent = `List Item ${count++}`;
+        list.appendChild(li);
+    }
+}
+
+// Add initial 10 items
+addItems(10);
+
+// Infinite Scroll
+window.addEventListener("scroll", () => {
+
+    if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight
+    ) {
+        addItems(2);
+    }
+
+});
